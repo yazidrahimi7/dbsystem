cdb77eb7e19e74d9df617401e6c89aefdd5dbfd4
Yazid <muhdyazid171997@gmail.com>
2020-08-05 22:23:43 +0800

    added basic login and logout

---
+++ login_verify.php
@@ -0,0 +1,26 @@
+<?php 
+include('connection.php');
+$var_username=$_POST['uname']; 
+$var_password=$_POST['pswd']; 
+
+$query =  "SELECT * FROM `login`";
+$result = mysqli_query ($connection, $query) or die ("SELECT Error ". mysqli_error($connection));
+	
+
+
+//checking success or not  
+if ($result && $var_username === 'admin' && $var_password === 'admin'){
+
+	header("location:home.php");
+	} else {
+			echo 
+			"<script> 
+			alert('Wrong username or password!');
+			window.location.href='index.php';
+			</script>";
+		
+	}
+
+mysqli_close($connection); 	
+?>
+?>
